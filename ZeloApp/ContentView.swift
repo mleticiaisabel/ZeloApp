46aeb335676ed2bc86b36001847415586d1e96b1
mleticiaisabel <mleticia.contact@gmail.com>
2026-06-25 09:54:16 -0300

    Initial Commit

---
+++ ZeloApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ZeloApp
+//
+//  Created by Maria Letícia Isabel Gonçalves  on 25/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ZeloApp/ZeloAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZeloAppApp.swift
+//  ZeloApp
+//
+//  Created by Maria Letícia Isabel Gonçalves  on 25/06/26.
+//
+
+import SwiftUI
+
+@main
+struct ZeloAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
