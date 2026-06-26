28dc70b625f95a6347c3757264a531ddabfbc416
Kemuel <economiakemuel@gmail.com>
2026-06-26 09:49:37 -0300

    alterações do nome da pagina de content view para Entrada do app

--- ZeloApp/EntradaDoAppView.swift
+++ ZeloApp/EntradaDoAppView.swift
@@ -1,15 +1,12 @@
 //
 //  ContentView.swift
 //  ZeloApp
-// oi
-// teste alysson
-// alynho
 //  Created by Maria Letícia Isabel Gonçalves  on 25/06/26.
 //
 
 import SwiftUI
 
-struct ContentView: View {
+struct EntradaDoAppView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -17,15 +14,13 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Hello, world!")
             
-            // bel aqui
-            //kad
+        
             
         }
         .padding()
-        // beloii
     }
 }
 
 #Preview {
-    ContentView()
+    EntradaDoAppView()
 }
