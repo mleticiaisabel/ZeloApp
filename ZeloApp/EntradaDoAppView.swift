e465db2baba2e9d0984fde7e8cfa33b6a75434cc
mleticiaisabel <mleticia.contact@gmail.com>
2026-07-01 09:57:38 -0300

    come;cando o codigo da tela principal

--- ZeloApp/EntradaDoAppView.swift
+++ ZeloApp/EntradaDoAppView.swift
@@ -7,27 +7,36 @@
 import SwiftUI
 
 struct EntradaDoAppView: View {
+    
     var body: some View {
         
-        ZStack {
-            LinearGradient(
-                colors: [
-                    Color.green.opacity(0.60),
-                    Color.green.opacity(0.1),
-                    Color(.clear)],
-                startPoint: .top,
-                endPoint: .center
-            )
-            .ignoresSafeArea()
+        NavigationStack {
             
-            VStack {
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
-                Text("Hello, world!")
+            ZStack {
+                LinearGradient(
+                    colors: [
+                        Color.green.opacity(0.60),
+                        Color.green.opacity(0.1),
+                        Color(.clear)],
+                    startPoint: .top,
+                    endPoint: .center
+                )
+                .ignoresSafeArea()
                 
+                VStack {
+                    Text(Date(),format: .dateTime.weekday(.wide).day().month(.wide))
+                        .font(.subheadline)
+                        .foregroundColor(.secondary)
+                        .environment(\.locale, Locale(identifier: "pt_BR"))
+                        .bold()
+                    
+                }
+                .navigationTitle("Saúde")
+                .navigationBarTitleDisplayMode(.automatic)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.horizontal, 19)
+                .padding(.top, -338)
             }
-            .padding()
         }
     }
 }
