3d07614825ad37c94a3233e45989720cf89bcfba
Alysson <alysson.anjos11@hotmail.com>
2026-07-02 11:12:39 -0300

    Fiz as telas doença e informação, e criei a informação da alergia para comeeçar a produzir ela

---
+++ ZeloApp/Alergia/InformacaoDaAlergia.swift
@@ -0,0 +1,18 @@
+//
+//  InformacaoDaAlergia.swift
+//  ZeloApp
+//
+//  Created by Alysson Lucas dos Anjos Silva on 02/07/26.
+//
+
+import SwiftUI
+
+struct InformacaoDaAlergia: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InformacaoDaAlergia()
+}
---
+++ ZeloApp/Doencas/InformacaoDaDoenca.swift
@@ -0,0 +1,18 @@
+//
+//  InformacaoDaDoenca.swift
+//  ZeloApp
+//
+//  Created by Alysson Lucas dos Anjos Silva on 02/07/26.
+//
+
+import SwiftUI
+
+struct InformacaoDaDoenca: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InformacaoDaDoenca()
+}
